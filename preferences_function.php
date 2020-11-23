ffd619dde387f169bd6bfa5db306dafb541926ef
rmbinder <r-m-b@gmx.net>
2020-11-23 15:01:41 +0100

    unnecessary back button after deinstallation

--- preferences_function.php
+++ preferences_function.php
@@ -123,7 +123,7 @@ case 2:
     
 case 3:
     
-	$gNavigation->addUrl(CURRENT_URL);
+	$gNavigation->clear();
 	$gMessage->setForwardUrl($gHomepage);		
 
 	$gMessage->show($gL10n->get('PLG_KATEGORIEREPORT_DEINST_STARTMESSAGE').$pPreferences->delete($_POST['deinst_org_select']) );
