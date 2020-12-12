5d92dd6328b0e8b1918769e11c31a358c3efb5d7
rmbinder <r-m-b@gmx.net>
2020-12-12 17:50:34 +0100

    update version

--- kategoriereport.php
+++ kategoriereport.php
@@ -3,7 +3,7 @@
  ***********************************************************************************************
  * Kategoriereport
  *
- * Version 3.0.0
+ * Version 3.1.0
  *
  * Dieses Plugin erzeugt eine Auflistung aller Rollenzugehoerigkeiten eines Mitglieds.
  * 
